bd498b04de6bd354074d2d3a05f8adfb775aac71
EnAnsari <rahmat2022a@gmail.com>
2024-08-15 23:05:12 +0330

    update: adding multi changes for project

--- bookstore/routes.js
+++ bookstore/routes.js
@@ -4,41 +4,42 @@ module.exports = (router) => {
         router.prefix('/books').setGroup(() => {
             router.setRoute('/:id', 'book-controller.readById').method('GET');
             router.setRoute('/', 'book-controller.readAll').method('GET');
-            router.setRoute('/', 'book-controller.create').method('POST');
+            router.setRoute('/', 'book-controller.create').method('POST')
+                .middleware('validationBook');
             router.setRoute('', 'book-controller.update').method('PUT');
             router.setRoute('/:id', 'book-controller.delete').method('DELETE');
             
         });
         
         router.prefix('tags').setGroup(function () {
-            router.setRoute('', 'tagController.readAll').method('GET');
-            router.setRoute('', 'tagController.create').method('POST');
-            router.setRoute('', 'tagController.update').method('PUT');
-            router.setRoute('/:id', 'tagController.delete').method('DELETE');
+            router.setRoute('', 'tag-controller.readAll').method('GET');
+            router.setRoute('', 'tag-controller.create').method('POST');
+            router.setRoute('', 'tag-controller.update').method('PUT');
+            router.setRoute('/:id', 'tag-controller.delete').method('DELETE');
 
         });
 
         router.prefix('comments').setGroup(function () {
-            router.setRoute('/:id', 'commentController.getComments').method('GET');
-            router.setRoute('', 'commentController.leaveComment').method('POST');
-            router.setRoute('/:id', 'commentController.delete').method('DELETE');
+            router.setRoute('/:id', 'comment-controller.getComments').method('GET');
+            router.setRoute('', 'comment-controller.leaveComment').method('POST');
+            router.setRoute('/:id', 'comment-controller.delete').method('DELETE');
 
         });
 
         router.prefix('users').setGroup(function () {
-            router.setRoute('/:id', 'userController.readById').method('GET');
-            router.setRoute('', 'userController.readAll').method('GET');
-            router.setRoute('', 'userController.create').method('POST');
-            router.setRoute('', 'userController.update').method('PUT');
-            router.setRoute('/:id', 'userController.delete').method('DELETE');
+            router.setRoute('/:id', 'user-controller.readById').method('GET');
+            router.setRoute('', 'user-controller.readAll').method('GET');
+            router.setRoute('', 'user-controller.create').method('POST');
+            router.setRoute('', 'user-controller.update').method('PUT');
+            router.setRoute('/:id', 'user-controller.delete').method('DELETE');
 
         });
 
         router.prefix('orders').setGroup(function () {
-            router.setRoute('/:id', 'orderController.readByUser').method('GET');
-            router.setRoute('', 'orderController.readAll').method('GET');
-            router.setRoute('', 'orderController.create').method('POST');
-            router.setRoute('', 'orderController.approval').method('PUT');
-            router.setRoute('/:id', 'orderController.delete').method('DELETE');
+            router.setRoute('/:id', 'order-controller.readByUser').method('GET');
+            router.setRoute('', 'order-controller.readAll').method('GET');
+            router.setRoute('', 'order-controller.create').method('POST');
+            router.setRoute('', 'order-controller.approval').method('PUT');
+            router.setRoute('/:id', 'order-controller.delete').method('DELETE');
         });
 };
