c556fce3d3a23dff715bda7e9a5a1baae6a04057
sajjad.dltkh <sajjad.d.1010@gmail.com>
2024-08-26 21:16:35 +0330

    complite the getConfirmedOrsers decorators

--- bookstore/decorators/after.js
+++ bookstore/decorators/after.js
@@ -1,32 +1,35 @@
 module.exports = class AfterDecorators {
-    
-  static async passwordDeleterForList(result) {
-    const usersList = result.data;
 
-    usersList.forEach((Element) => {
-      delete Element.body.password;
-    });
+    static async passwordDeleterForList(result) {
+        const usersList = result.data;
 
-    return usersList;
-  }
+        usersList.forEach((Element) => {
+            delete Element.body.password;
+        });
 
-  static async passwordDeleterForOne(result) {
-    delete result.data.body.password;
+        return usersList;
+    }
 
-    return result;
-  }
+    static async passwordDeleterForOne(result) {
+        delete result.data.body.password;
 
-  static async getConfirmedOrders(result) {
-    // TODO: implement your code ...
+        return result;
+    }
 
-    return result;
-  }
+    static async getConfirmedOrders(result) {
 
-  static async getPublishedComments(result) {
-    // TODO: implement your code ...
+        const oerdersList = result.data
+       
+        const confirmedOrders = oerdersList.filter(item => item.details.confirmed);
 
-    return result;
-  }
+        return confirmedOrders
+    }
+
+    static async getPublishedComments(result) {
+        // TODO: implement your code ...
+
+        return result;
+    }
 };
 
 // function passwordDeleterForList(result) {
